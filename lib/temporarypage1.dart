d314cb1f47d0b7537bf75dbcc101d560f3f756e0
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-10-11 22:06:52 +0500

    Just nothing 1

--- lib/temporarypage1.dart
+++ lib/temporarypage1.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basics/reusable.dart';
 
 void main()=>runApp(
   MaterialApp(
     debugShowCheckedModeBanner: false,
-
+    home: MyApp(),
   ),
 );
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
