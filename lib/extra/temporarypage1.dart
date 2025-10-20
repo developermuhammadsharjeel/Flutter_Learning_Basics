71c068ddc951f5f4d7d1327f9e2ea78f24fb80ff
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-10-20 11:24:07 +0500

    Practise

--- lib/extra/temporarypage1.dart
+++ lib/extra/temporarypage1.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basics/reusable_widget.dart';
+import 'package:flutter_basics/MadProject/main.dart';
 
 void main()=>runApp(
   MaterialApp(
@@ -8,32 +8,66 @@ void main()=>runApp(
   ),
 );
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
+      appBar: AppBar(
+        leading: Icon(Icons.add_business),
+        title: Text("Sample"),
+        actions: [
+          Icon(Icons.search)
+        ],
+      ),
+
+      body: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+
             decoration: BoxDecoration(
-              color: Colors.grey,
-              boxShadow: [BoxShadow(
-                spreadRadius: 3,
-                blurRadius: 15,
-                color: Colors.black,
-                offset: Offset(0.3, 0.4),
-              )
+              color: Color.fromRGBO(100, 100, 255, 0.3),
+              borderRadius: BorderRadius.circular(25),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  blurRadius: 10,
+                  spreadRadius: 5,
+                ),
               ],
             ),
-            child: Text("Sample Data", style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Times New Roman',
-            ),),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                    borderRadius: BorderRadius.circular(30),
+                    ),
+
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
 }
+
+
